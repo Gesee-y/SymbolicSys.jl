5f49374af08a95087398dd10ddfc2997c935fe43
Gesee-y <gesee37@gmail.com>
2025-03-15 00:06:50 +0100

    Update operations.jl

--- src/operations.jl
+++ src/operations.jl
@@ -23,6 +23,6 @@ derivate(n::AddNode) = AddNode(derivate(n.n1), derivate(n.n2))
 derivate(n::SubNode) = SubNode(derivate(n.n1), derivate(n.n2))
 derivate(n::ProdNode) = AddNode(ProdNode(derivate(n.n1), n.n2), ProdNode(n.n1, derivate(n.n2)))
 derivate(n::DivNode) = DivNode(SubNode(ProdNode(derivate(n.n1), n.n2), ProdNode(n.n1, derivate(n.n2))), PowNode(n[2], ConstNode(2)))
-derivate(n::PowNode) = ProdNode(PowNode(n.n1,n.n2) AddNode(ProdNode(derivate(n.n2), LnNode(n.n1)), )
+derivate(n::PowNode) = ProdNode(PowNode(n.n1,n.n2) AddNode(ProdNode(derivate(n.n2), LnNode(n.n1)), DivNode(ProdNode(n.n2, derivate(n.n1)), n.n1))
 
-derivate(tree::NSyntaxTree) = NSyntaxTree(deriva
+derivate(tree::NSyntaxTree) = NSyntaxTree(derivate(tree.root))
