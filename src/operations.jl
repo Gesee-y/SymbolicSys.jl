81af3f61ea9a009d803deb7e42a445089f2cf754
Talom Laël <gesee37@gmail.com>
2025-04-09 08:58:13 +0100

    Update operations.jl

--- src/operations.jl
+++ src/operations.jl
@@ -24,13 +24,19 @@ derivate(n::Number) = 0
 derivate(n::ConstNode) = ConstNode(0)
 derivate(n::SymbNode) = ConstNode(1)
 
-derivate(n::AddNode) = AddNode(derivate(n.n1), derivate(n.n2))
-derivate(n::SubNode) = SubNode(derivate(n.n1), derivate(n.n2))
-derivate(n::ProdNode) = AddNode(ProdNode(derivate(n.n1), n.n2), ProdNode(n.n1, derivate(n.n2)))
-derivate(n::DivNode) = DivNode(SubNode(ProdNode(derivate(n.n1), n.n2), ProdNode(n.n1, derivate(n.n2))), PowNode(n[2], ConstNode(2)))
-derivate(n::PowNode) = ProdNode(PowNode(n.n1,n.n2), AddNode(ProdNode(derivate(n.n2), LnNode(n.n1)), DivNode(ProdNode(n.n2, derivate(n.n1)), n.n1)))
-derivate(n::LnNode) = DivNode(derivate(n.n), n.n)
-derivate(n::LogNode) = DivNode(derivate(n.n), ProdNode(n.n, ConstNode(log(10))))
+derivate(n::AddNode) = derivate(n.n1) + derivate(n.n2)
+derivate(n::SubNode) = derivate(n.n1) - derivate(n.n2)
+derivate(n::ProdNode) = derivate(n.n1)*n.n2 + n.n1*derivate(n.n2)
+derivate(n::DivNode) = (derivate(n.n1)*n.n2 - n.n1*derivate(n.n2))/ n[2]^2
+derivate(n::PowNode) = begin 
+    if n.n2 isa ConstNode
+        return n.n2*derivate(n.n1)*n.n1^(n.n2.n-1)
+    else
+        return n.n1^n.n2 * (derivate(n.n2)*LnNode(n.n1) + n.n2* derivate(n.n1))/n.n1
+    end
+end
+derivate(n::LnNode) = derivate(n.n)/n.n
+derivate(n::LogNode) = derivate(n.n)/ (n.n*log(10))
 
 derivate(tree::NSyntaxTree) = NSyntaxTree(derivate(tree.root))
 
